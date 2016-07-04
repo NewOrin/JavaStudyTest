c12bd3a34d7252e8e78f9f8830313c26558d371a
NewOrin Zhang <NewOrinZhang@Gmail.com>
2016-07-04 21:14:31 +0800

    线程Demo

---
+++ thread-in-imooc/src/com/imooc/concurrent/base/KeyPersonThread.java
@@ -0,0 +1,16 @@
+package com.imooc.concurrent.base;
+/**
+ * 农民军线程
+ * @author Neworin
+ *
+ */
+public class KeyPersonThread extends Thread {
+
+	public void run(){
+		System.out.println(Thread.currentThread().getName()+"开始了战斗！");
+		for(int i=0;i<10;i++){
+			System.out.println(Thread.currentThread().getName()+"左突右杀,攻击隋军...");
+		}
+		System.out.println(Thread.currentThread().getName()+"结束了战斗！");
+	}
+}
